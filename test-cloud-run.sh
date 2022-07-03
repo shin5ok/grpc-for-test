cbfaea5b59ac06643c46bac0937e3af7f42d74a0
shin5ok <shin5ok@55mp.com>
2022-07-04 01:00:09 +0900

    add cli to test it easily

--- test-cloud-run.sh
+++ test-cloud-run.sh
@@ -8,3 +8,4 @@ grpcurl -vv -proto proto/simple.proto -d '{"message":"test message"}' ${CLOUD_RU
 # for Cloud Run service required auth
 TOKEN=$(gcloud auth print-identity-token)
 grpcurl -H "Authorization: Bearer $TOKEN" -proto proto/simple.proto ${CLOUD_RUN_HOST}:443 simple.Simple.PingPong
+grpcurl -H "Authorization: Bearer $TOKEN" -proto proto/simple.proto ${CLOUD_RUN_HOST}:443 simple.Simple.ListMessage
