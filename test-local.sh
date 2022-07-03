03f38daa7b33a25a65df0786a4d613f45a78999a
shin5ok <shin5ok@55mp.com>
2022-07-03 23:20:28 +0900

    modify the type of the arg

--- test-local.sh
+++ test-local.sh
@@ -1,3 +1,3 @@
-grpcurl -plaintext -d '{"id":"v"}' localhost:8080 simple.Simple.GetMessage
+grpcurl -plaintext -d '{"id":100}' localhost:8080 simple.Simple.GetMessage
 grpcurl -plaintext -d '{"message":"foo"}' localhost:8080 simple.Simple.PutMessage
 grpcurl -plaintext localhost:8080 simple.Simple.PingPong
