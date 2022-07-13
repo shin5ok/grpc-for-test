55a0c24a7244c566eb8f2ed9f8dd40a1e6be9e7d
shin5ok <shin5ok@55mp.com>
2022-07-13 22:34:03 +0900

    re-arrange utils

---
+++ utils/test-local.sh
@@ -0,0 +1,4 @@
+grpcurl -plaintext -d '{"id":100}' localhost:8080 simple.Simple.GetMessage
+grpcurl -plaintext -d '{"message":"foo"}' localhost:8080 simple.Simple.PutMessage
+grpcurl -plaintext localhost:8080 simple.Simple.PingPong
+grpcurl -plaintext localhost:8080 simple.Simple.ListMessage
