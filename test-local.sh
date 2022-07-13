b0ca31e1fa26f29ab45ce12361ee2072bc9d6225
shin5ok <shin5ok@55mp.com>
2022-07-13 18:03:54 +0900

    move test scripts

--- test-local.sh
+++
@@ -1,4 +0,0 @@
-grpcurl -plaintext -d '{"id":100}' localhost:8080 simple.Simple.GetMessage
-grpcurl -plaintext -d '{"message":"foo"}' localhost:8080 simple.Simple.PutMessage
-grpcurl -plaintext localhost:8080 simple.Simple.PingPong
-grpcurl -plaintext localhost:8080 simple.Simple.ListMessage
